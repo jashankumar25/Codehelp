29158b1ffe40e4ee1cd14bbc7072f9b1cbcd0a5b
jashankumar <jashanbansal2004@gmail.com>
2026-06-28 15:21:20 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.35%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,6 +1,33 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        // Approach 3: Dutch National Flag Algorithm (Optimal)
+        int i = 0;
+        int j = 0;
+        int k = nums.size() - 1;
+
+        while (j <= k) {
+            if (nums[j] == 1) {
+                j++;
+            } else if (nums[j] == 2) {
+                swap(nums[j], nums[k]);
+                k--;
+            } else {
+                swap(nums[i], nums[j]);
+                i++;
+                j++;
+            }
+        }
+    }
+};
+
+/*
+---------------------------------------
+ void sortColors(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+    }
+--------------------------------------------
+void sortColors(vector<int>& nums) {
         int one = 0, zero = 0, two = 0;
         for (int i = 0; i < nums.size(); i++) {
 
@@ -28,11 +55,4 @@ public:
             two--;
         }
     }
-};
-/*
----------------------------------------
- void sortColors(vector<int>& nums) {
-        sort(nums.begin(),nums.end());
-    }
---------------------------------------------
 */
