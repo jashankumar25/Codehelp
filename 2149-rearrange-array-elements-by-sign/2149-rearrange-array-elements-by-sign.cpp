eaa9133aa6fd117bd9fd4a59f6ecd10a384b5d08
jashankumar <jashanbansal2004@gmail.com>
2026-06-28 21:56:05 +0530

    Update 2149-rearrange-array-elements-by-sign.cpp

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.cpp
@@ -19,6 +19,43 @@ public:
         return ans;
     }
 };
+/*class Solution {
+public:
+    vector<int> rearrangeArray(vector<int>& nums) {
+
+        int n = nums.size();
+
+        // Step 1 : Partition
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+
+            while (left < n && nums[left] > 0)
+                left++;
+
+            while (right >= 0 && nums[right] < 0)
+                right--;
+
+            if (left < right)
+                swap(nums[left], nums[right]);
+        }
+
+        // Step 2 : Rearrange
+        int pos = 1;
+        int neg = n / 2;
+
+        while (pos < n && neg < n) {
+
+            swap(nums[pos], nums[neg]);
+
+            pos += 2;
+            neg++;
+        }
+
+        return nums;
+    }
+};*/
 /*vector<int> rearrangeArray(vector<int>& nums) {
         vector<int> arr1;
         vector<int> arr2;
