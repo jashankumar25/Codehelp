7765b36d0efa05d089c3496f0e4289e86bdf9dbc
jashankumar <jashanbansal2004@gmail.com>
2026-06-28 20:17:14 +0530

    Update 0121-best-time-to-buy-and-sell-stock.cpp

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.cpp
@@ -12,3 +12,38 @@ public:
         return profit;
     }
 };
+/*------------------------------------------
+int maxProfit(vector<int>& prices) {
+
+        int curr = 0;
+        int ans = 0;
+
+        for(int i=1;i<prices.size();i++)
+        {
+            curr += prices[i]-prices[i-1];
+
+            if(curr<0)
+                curr=0;
+
+            ans=max(ans,curr);
+        }
+
+        return ans;
+    }
+ --------------------------------------------------------      
+ int maxProfit(vector<int>& prices) {
+
+        int ans = 0;
+
+        for(int i=0;i<prices.size();i++)
+        {
+            for(int j=i+1;j<prices.size();j++)
+            {
+                ans=max(ans,prices[j]-prices[i]);
+            }
+        }
+
+        return ans;
+    }
+
+*/
