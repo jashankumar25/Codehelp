ed9a40184a0057bb93478b2c980826ab5a1e1984
jashankumar <jashanbansal2004@gmail.com>
2026-07-09 22:27:10 +0530

    Time: 8 ms (49.76%), Space: 11.3 MB (86.41%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,6 +9,33 @@
 class Solution {
 public:
     ListNode* detectCycle(ListNode* head) {
+        ListNode* slow = head;
+        ListNode* fast = head;
+
+        while (fast && fast->next) {
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if (slow == fast) // point of intersections
+            {
+                break;
+            }
+        }
+        if (!fast || !fast->next) {
+            return nullptr;
+        }
+         slow = head;
+
+        while (slow != fast)
+        {
+            slow = slow->next;
+            fast = fast->next;
+        }
+
+        return slow;
+    }
+};
+/*ListNode* detectCycle(ListNode* head) {
 
         if (head == NULL) {
             return NULL;
@@ -24,5 +51,4 @@ public:
             temp = temp->next;
         }
         return NULL;
-    }
-};
+    }*/
