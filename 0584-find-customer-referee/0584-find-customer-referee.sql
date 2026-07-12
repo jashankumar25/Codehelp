7a0765976b81f18e5a5d73b50fd5d94528d0eb4b
jashankumar <jashanbansal2004@gmail.com>
2026-07-13 01:45:40 +0530

    Time: 455 ms (95.94%), Space: 0B (100%) - LeetHub

---
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -0,0 +1,14 @@
+# Write your MySQL query statement below
+SELECT 
+     name FROM Customer WHERE
+IFNULL(referee_id,0)!=2;
+
+#<> Is same as !=
+/*
+------------------------
+SELECT
+   name
+FROM
+   Customer
+WHERE
+    referee_id IS NULL OR referee_id!=2 ;*/
