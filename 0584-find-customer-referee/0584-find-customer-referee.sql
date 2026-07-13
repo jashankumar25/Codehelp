747b30d1a9a75921044519fa2e43fd108083c014
jashankumar <jashanbansal2004@gmail.com>
2026-07-13 18:42:16 +0530

    Time: 602 ms (12.92%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,14 +1,11 @@
 # Write your MySQL query statement below
-SELECT 
-     name FROM Customer WHERE
-IFNULL(referee_id,0)!=2;
+select name from Customer where referee_id!=2 or referee_id is  null;
+
+-- SELECT: Return only the name column.
+-- FROM: Read data from the Customer table.
+-- WHERE: Select customers whose referee_id is NOT 2.
+-- OR: Also include customers with no referee (NULL). '=' cannot compare NULL, so use IS NULL.
+
+-- SELECT name FROM Customer WHERE IFNULL(referee_id,0)!=2;
 
 #<> Is same as !=
-/*
-------------------------
-SELECT
-   name
-FROM
-   Customer
-WHERE
-    referee_id IS NULL OR referee_id!=2 ;*/
