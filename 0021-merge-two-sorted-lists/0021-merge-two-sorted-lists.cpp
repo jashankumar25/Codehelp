3f1146faf853a3ef1ecc0f4c2c87b3b8759f6a95
jashankumar <jashanbansal2004@gmail.com>
2026-07-16 10:54:19 +0530

    Time: 0 ms (100%), Space: 19.6 MB (27.94%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.cpp
@@ -8,49 +8,40 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
-void solve(ListNode* list1, ListNode* list2) {
-
-    //if only one elemnt 
-    if(list1->next==nullptr)
-    {
-        list1->next=list2;
-        return;
-    }
-    
-    ListNode* curr1 = list1;
-    ListNode* next1 = curr1->next;
-
-    ListNode* curr2 = list2;
-    ListNode* next2 = curr2->next;
-
-    while (curr2 != nullptr && next1 != nullptr) {
-        if (curr1->val <= curr2->val && curr2->val <= next1->val) {
-            next2 = curr2->next;
-
-            curr1->next = curr2;
-            curr2->next = next1;
-
-            curr1 = curr2;
-            curr2 = next2;
+class Solution {
+public:
+    void solve(ListNode* list1, ListNode* list2) {
+        if (list1->next == nullptr) {
+            list1->next = list2;
+            return;
         }
+        ListNode* curr1 = list1;
+        ListNode* next1 = curr1->next;
 
-        else {
-            // curr1 and next1 ko aage badhana padega
-            curr1 = next1;
-            next1 = curr1->next;
+        ListNode* curr2 = list2;
+        ListNode* next2 = curr2->next;
 
-            if (next1 == nullptr) {
+        while (next1 != nullptr && curr2 != nullptr) {
+            if (curr2->val >= curr1->val && curr2->val <= next1->val) {
+                next2 = curr2->next;
+
+                curr2->next = next1;
                 curr1->next = curr2;
-                return;
+
+                curr1 = curr2;
+                curr2 = next2;
+            } else {
+                curr1 = next1;
+                next1 = curr1->next;
+
+                if (next1 == nullptr) {
+                    curr1->next = curr2;
+                    return;
+                }
             }
         }
     }
-}
-
-class Solution {
-public:
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-
         if (list1 == nullptr)
             return list2;
 
@@ -64,7 +55,6 @@ public:
             solve(list2, list1);
             return list2;
         }
-
         return list1;
     }
 };
