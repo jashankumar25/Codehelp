256a623a233ae203022daa1533125e5bafb754d4
jashankumar <jashanbansal2004@gmail.com>
2026-07-23 15:19:19 +0530

    Time: 0 ms (100%), Space: 14.4 MB (33.14%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,30 +1,26 @@
-class Solution
-{
+class Solution {
 public:
-    string removeDuplicates(string s)
-    {
-        // intialise ans string as empty str
-        string ans = "";
-        int n = s.length();
+    string removeDuplicates(string s) {
 
-        for (int i = 0; i < n; i++)
+        stack<char> st;
+
+        for(char ch : s)
         {
-            char currcharacter = s[i];
-            if (ans.empty())
-            {
-                // if ans is empty,,seedha push karo
-                ans.push_back(currcharacter);
-            }
-            // rightmost character of ans=ans.back()
-            else if (currcharacter == ans.back())
-            {
-                ans.pop_back();
-            }
+            if(!st.empty() && st.top() == ch)
+                st.pop();
             else
-            {
-                ans.push_back(currcharacter);
-            }
+                st.push(ch);
         }
+
+        string ans = "";
+
+        while(!st.empty())
+        {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
 
         return ans;
     }
