a8ac53e792c45a33f29366bc27440b33dbf56603
jashankumar <jashanbansal2004@gmail.com>
2026-07-23 15:06:19 +0530

    Time: 11 ms (26.77%), Space: 13.6 MB (73.34%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,6 +1,31 @@
-class Solution {
+class Solution
+{
 public:
-    string removeDuplicates(string s) {
-        
+    string removeDuplicates(string s)
+    {
+        // intialise ans string as empty str
+        string ans = "";
+        int n = s.length();
+
+        for (int i = 0; i < n; i++)
+        {
+            char currcharacter = s[i];
+            if (ans.empty())
+            {
+                // if ans is empty,,seedha push karo
+                ans.push_back(currcharacter);
+            }
+            // rightmost character of ans=ans.back()
+            else if (currcharacter == ans.back())
+            {
+                ans.pop_back();
+            }
+            else
+            {
+                ans.push_back(currcharacter);
+            }
+        }
+
+        return ans;
     }
 };
