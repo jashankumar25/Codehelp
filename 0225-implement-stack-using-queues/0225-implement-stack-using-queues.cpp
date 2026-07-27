556adcf7f84d88c92116381ea2634273ee4275bb
jashankumar <jashanbansal2004@gmail.com>
2026-07-27 13:56:07 +0530

    Update 0225-implement-stack-using-queues.cpp

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.cpp
@@ -1,5 +1,30 @@
 class MyStack {
 public:
+    queue<int> q;
+    MyStack() {}
+
+    void push(int x) {
+        q.push(x);
+        for (int i = 0; i < q.size() - 1; i++) {
+            q.push(q.front());
+            q.pop();
+        }
+    }
+
+    int pop() {
+        int x = q.front();
+        q.pop();
+        return x;
+    }
+
+    int top() { return q.front(); }
+
+    bool empty() { return q.empty(); }
+};
+
+/**
+  class MyStack {
+public:
     queue<int> q1;
     queue<int> q2;
 
@@ -25,8 +50,6 @@ public:
 
     bool empty() { return q1.empty(); }
 };
-
-/**
  * Your MyStack object will be instantiated and called as such:
  * MyStack* obj = new MyStack();
  * obj->push(x);
