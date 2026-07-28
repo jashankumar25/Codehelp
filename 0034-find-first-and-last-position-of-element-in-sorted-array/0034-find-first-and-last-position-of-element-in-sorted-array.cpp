b182e3e6f94669c09ed2d3fc3973269edab7b034
jashankumar <jashanbansal2004@gmail.com>
2026-07-29 04:09:42 +0530

    Time: 0 ms (100%), Space: 17.7 MB (20.44%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,15 +1,71 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& arr, int target) {
 
-        int indxlower =
-            lower_bound(arr.begin(), arr.end(), target) - arr.begin();
-        int indxupper =
-            upper_bound(arr.begin(), arr.end(), target) - arr.begin() - 1;
+    int firstOccurrence(vector<int>& nums,int target)
+    {
+        int low=0;
+        int high=nums.size()-1;
+        int ans=-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
 
-        if (indxlower == arr.size() || arr[indxlower] != target)
-            return {-1, -1};
+            if(nums[mid]==target)
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
 
-        return {indxlower, indxupper};
+        return ans;
+    }
+
+    int lastOccurrence(vector<int>& nums,int target)
+    {
+        int low=0;
+        int high=nums.size()-1;
+
+        int ans=-1;
+
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]==target)
+            {
+                ans=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]<target)
+                low=mid+1;
+            else
+                high=mid-1;
+        }
+
+        return ans;
+    }
+
+    vector<int> searchRange(vector<int>& nums, int target)
+    {
+        return {firstOccurrence(nums,target),
+                lastOccurrence(nums,target)};
     }
 };
+/*
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int first = -1, sec = -1;
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] == target) {
+                if (first == -1)
+                    first = i;
+
+                        sec = i;
+            }
+        }
+        return {first, sec};
+    }
+*/
