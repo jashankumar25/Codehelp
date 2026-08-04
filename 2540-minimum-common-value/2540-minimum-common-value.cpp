9b734196ef6cf32c8c06befae6eb60821393bc02
jashan kumar <jashanbansal2004@gmail.com>
2026-08-04 14:04:36 +0530

    Time: 43 ms (14.2%), Space: 69.6 MB (13.63%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.cpp
+++ 2540-minimum-common-value/2540-minimum-common-value.cpp
@@ -1,17 +1,26 @@
 class Solution {
 public:
     int getCommon(vector<int>& nums1, vector<int>& nums2) {
-        int i = 0, j = 0;
+        unordered_set<int> st(nums1.begin(), nums1.end());
 
-        while (i < nums1.size() && j < nums2.size()) {
-            if (nums1[i] == nums2[j]) {
-                return nums1[i];
+        for (auto num : nums2) {
+            if (st.find(num) != st.end()) {
+                return num;
             }
-            if (nums1[i] < nums2[j]) {
-                i++;
-            } else
-                j++;
         }
         return -1;
     }
-};
+}; /*int getCommon(vector<int>& nums1, vector<int>& nums2) {
+         int i = 0, j = 0;
+
+         while (i < nums1.size() && j < nums2.size()) {
+             if (nums1[i] == nums2[j]) {
+                 return nums1[i];
+             }
+             if (nums1[i] < nums2[j]) {
+                 i++;
+             } else
+                 j++;
+         }
+         return -1;
+     }*/
